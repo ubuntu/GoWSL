5b7c148874458daa2b4807b0489cd2277e1b247b
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-11-10 14:01:34 +0100

    Added more functionality to tests

--- WslApi/main_test.go
+++ WslApi/main_test.go
@@ -3,10 +3,13 @@ package WslApi_test
 import (
 	"WslApi"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -16,47 +19,69 @@ const (
 
 type Tester struct {
 	*testing.T
-	distros *[]WslApi.Distro
+	distros []WslApi.Distro
+	tmpdirs []string
 }
 
 func TestMain(m *testing.M) {
 	WslApi.Shutdown()
-	cleanupAllDistros()
+	cleanUpTestDistros()
 
 	exitVal := m.Run()
 
 	WslApi.Shutdown()
-	cleanupAllDistros()
+	cleanUpTestDistros()
 
 	os.Exit(exitVal)
 }
 
 // NewTester extends Tester with some WSL-specific functionality and cleanup
-func NewTester(tst *testing.T) (t Tester) {
-	distros := new([]WslApi.Distro)
-	t = Tester{T: tst, distros: distros}
+func NewTester(tst *testing.T) *Tester {
+	t := Tester{T: tst}
 	t.Cleanup(func() {
-		cleanUpDistros(*distros)
+		t.cleanUpDistros()
+		t.cleanUpTempDirectories()
 	})
-	return t
+	return &t
 }
 
 // NewDistro creates a new distro with a mangled name and adds it to list of distros to remove.
 // Note that the distro is not registered.
 func (t *Tester) NewDistro(name string) WslApi.Distro {
 	d := WslApi.Distro{Name: t.mangleName(name)}
-	*t.distros = append(*t.distros, d)
+	t.distros = append(t.distros, d)
 	return d
 }
 
-// mangleName avoids name collisions with existing distros by adding a suffix
-func (t Tester) mangleName(name string) string {
-	return fmt.Sprintf("%s_%s_%s", name, t.Name(), distroSuffix)
+// NewTestDir creates a unique directory and adds it to the list of dirs to remove
+func (t *Tester) NewTestDir(prefix string) (string, error) {
+	clean_prefix := strings.Replace(t.Name()+prefix, "/", "_", -1)
+	tmpdir, err := ioutil.TempDir(os.TempDir(), clean_prefix)
+	if err != nil {
+		return "", err
+	}
+
+	t.tmpdirs = append(t.tmpdirs, tmpdir)
+	return tmpdir, nil
 }
 
-// cleanupAllDistros finds all distros with a mangled name and unregisters them
-func cleanupAllDistros() {
-	testDistros, err := findTestDistros()
+func (t *Tester) cleanUpDistros() {
+	cleanUpDistros(t.distros)
+}
+
+func (t *Tester) cleanUpTempDirectories() {
+	for _, dir := range t.tmpdirs {
+		dir := dir
+		go func() {
+			err := os.RemoveAll(dir)
+			t.Logf("Failed to remove temp directory %s: %v", dir, err)
+		}()
+	}
+}
+
+// cleanUpTestDistros finds all distros with a mangled name and unregisters them
+func cleanUpTestDistros() {
+	testDistros, err := RegisteredTestDistros()
 	if err != nil {
 		return
 	}
@@ -68,29 +93,21 @@ func cleanupAllDistros() {
 
 func cleanUpDistros(distros []WslApi.Distro) {
 	for _, distro := range distros {
-		if r, err := distro.IsRegistered(); err == nil && !r {
-			continue
-		}
 
+		if r, err := distro.IsRegistered(); err == nil && !r {
+			return
+		}
 		err := distro.Unregister()
 		if err != nil {
 			name, test := unmangleName(distro.Name)
-			fmt.Printf("failed to clean up test distro (name=%s, test=%s)\n", name, test)
+			fmt.Printf("Failed to clean up test distro (name=%s, test=%s)\n", name, test)
 		}
+
 	}
 }
 
-// unmangleName retrieves encoded info from distro name
-func unmangleName(mangledName string) (name string, test string) {
-	words := strings.Split(mangledName, "_")
-	l := len(words)
-	name = strings.Join(words[:l-2], "_")
-	test = words[l-2]
-	return name, test
-}
-
-// findTestDistros finds all distros with a mangled name
-func findTestDistros() ([]WslApi.Distro, error) {
+// RegisteredTestDistros finds all distros with a mangled name
+func RegisteredTestDistros() ([]WslApi.Distro, error) {
 	distros := []WslApi.Distro{}
 
 	outp, err := exec.Command("powershell.exe", "-command", "$env:WSL_UTF8=1 ; wsl.exe --list --quiet").CombinedOutput()
@@ -106,4 +123,27 @@ func findTestDistros() ([]WslApi.Distro, error) {
 	}
 
 	return distros, nil
+}
+
+// mangleName avoids name collisions with existing distros by adding a suffix
+func (t Tester) mangleName(name string) string {
+	return fmt.Sprintf("%s_%s_%s", name, strings.ReplaceAll(t.Name(), "/", "--"), distroSuffix)
+}
+
+// unmangleName retrieves encoded info from distro name
+func unmangleName(mangledName string) (name string, test string) {
+	words := strings.Split(mangledName, "_")
+	l := len(words)
+	name = strings.Join(words[:l-2], "_")
+	test = words[l-2]
+	return name, test
+}
+
+// registerViaCommandline registers a distro bypassing the WslApi module, for better test segmentation
+func registerViaCommandline(t *Tester, distro WslApi.Distro) {
+	tmpdir, err := t.NewTestDir(distro.Name)
+	require.NoError(t, err)
+
+	str, err := exec.Command("wsl.exe", "--import", distro.Name, tmpdir, jammyRootFs).CombinedOutput()
+	require.NoError(t, err, str)
 }

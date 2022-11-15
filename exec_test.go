ca14fd7180621fed98d0fcc345c631de1d236a9b
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-11-15 10:00:05 +0100

    Improved exec tests

--- exec_test.go
+++ exec_test.go
@@ -9,16 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExitStatusSuccess(tst *testing.T) {
+func TestCommandExitStatusSuccess(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
 	// Test that exit values are returned correctly
-	err := d.Command("exit 0").Run()
+	cmd := d.Command("exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
 	require.NoError(t, err)
 }
 
-func TestNoExistExecutable(tst *testing.T) {
+func TestCommandNoExistExecutable(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
@@ -34,12 +37,15 @@ func TestNoExistExecutable(tst *testing.T) {
 	require.Equal(t, errAsExitError.Code, wsl.ExitCode(127)) // 127: command not found
 }
 
-func TestExitStatusFailed(tst *testing.T) {
+func TestCommandExitStatusFailed(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
 	// Test that exit values are returned correctly
-	err := d.Command("exit 42").Run()
+	cmd := d.Command("exit 42")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
 	require.Error(t, err)
 
 	var errAsExitError *wsl.ExitError
@@ -47,13 +53,17 @@ func TestExitStatusFailed(tst *testing.T) {
 	require.Equal(t, errAsExitError.Code, wsl.ExitCode(42))
 }
 
-func TestFailureNoDistro(tst *testing.T) {
+func TestCommandFailureNoDistro(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.NewWslDistro("ubuntu")
 	// We do not register it
 
 	// Test that exit values are returned correctly
-	err := d.Command("exit 0").Run()
+	cmd := d.Command("exit 0")
+	cmd.Stderr = 0
+	cmd.Stdout = 0
+
+	err := cmd.Run()
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "something went wrong Windows-side")
 
@@ -61,56 +71,118 @@ func TestFailureNoDistro(tst *testing.T) {
 	require.NotErrorIs(t, err, errAsExitError)
 }
 
-// TestTimeoutEarlyFailure tests no error is returned if the command finishes on time
-func TestTimeoutSuccess(tst *testing.T) {
+// TestCommandTimeoutSuccess tests no error is returned if the command finishes on time
+func TestCommandTimeoutSuccess(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
 	// Poking distro to wake it up
-	err := d.Command("exit 0").Run()
+	cmd := d.Command("exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
 	require.NoError(t, err)
 
 	// Actual test
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err = d.CommandContext(ctx, "exit 0").Run()
+	cmd = d.CommandContext(ctx, "exit 0")
+	cmd.Stderr = 0
+	cmd.Stdout = 0
+	err = cmd.Run()
 	require.NoError(t, err)
 }
 
-// TestTimeoutEarlyFailure tests behaviour when timing out before command is launched
-func TestTimeoutEarlyFailure(tst *testing.T) {
+// TestCommandTimeoutEarlyFailure tests behaviour when timing out before command is launched
+func TestCommandTimeoutEarlyFailure(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
-	// Actual test
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
 	defer cancel()
 
-	time.Sleep(2 * time.Millisecond)
-	t.Logf("Started\n")
-	err := d.CommandContext(ctx, "exit 0").Run()
-	t.Logf("Finished\n")
+	time.Sleep(10 * time.Millisecond)
+
+	cmd := d.CommandContext(ctx, "exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "context deadline exceeded")
 }
 
-// TestTimeoutLateFailure tests behaviour when timing out after command is launched
-func TestTimeoutLateFailure(tst *testing.T) {
+// TestCommandTimeoutLateFailure tests behaviour when timing out after command is launched
+func TestCommandTimeoutLateFailure(tst *testing.T) {
 	t := NewTester(tst)
 	d := t.CachedDistro()
 
 	// Poking distro to wake it up
-	err := d.Command("exit 0").Run()
+	cmd := d.Command("exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
 	require.NoError(t, err)
 
 	// Actual test
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	t.Logf("Started\n")
-	err = d.CommandContext(ctx, "sleep 5 && exit 0").Run()
-	t.Logf("Finished\n")
+	cmd = d.CommandContext(ctx, "sleep 5 && exit 0")
+	cmd.Stderr = 0
+	cmd.Stdout = 0
+	err = cmd.Run()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "process was closed before finshing")
+}
+
+// TestCommandCancelSuccess tests no error is returned if the command finishes on time
+func TestCommandCancelSuccess(tst *testing.T) {
+	t := NewTester(tst)
+	d := t.CachedDistro()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cmd := d.CommandContext(ctx, "exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
+	require.NoError(t, err)
+}
+
+// TestCommandCancelEarlyFailure tests behaviour when timing out before command is launched
+func TestCommandCancelEarlyFailure(tst *testing.T) {
+	t := NewTester(tst)
+	d := t.CachedDistro()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	cmd := d.CommandContext(ctx, "exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Run()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "context canceled")
+}
+
+// TestCommandCancelLateFailure tests behaviour when timing out after command is launched
+func TestCommandCancelLateFailure(tst *testing.T) {
+	t := NewTester(tst)
+	d := t.CachedDistro()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cmd := d.CommandContext(ctx, "exit 0")
+	cmd.Stdout = 0
+	cmd.Stderr = 0
+	err := cmd.Start()
+	require.NoError(t, err)
+
+	cancel()
+
+	err = cmd.Wait()
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "process was closed before finshing")
 }

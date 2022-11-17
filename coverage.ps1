88fa54ef3178f4fb33b747bdac964620acafd94a
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-11-17 16:57:14 +0100

    Fixed coverage script

--- coverage.ps1
+++ coverage.ps1
@@ -1,2 +1,3 @@
-go.exe test -covermode=set -coverprofile="${cov_dir}/coverage.out" -coverpkg=./... $@
-go.exe tool cover -o "${cov_dir}/index.html" -html="${cov_dir}/coverage.out.filtered"
+$cov_dir=".\coverage"
+go.exe test -covermode=set -coverprofile="${cov_dir}\coverage.out" -coverpkg=./...
+go.exe tool cover -o "${cov_dir}\index.html" -html="${cov_dir}\coverage.out"

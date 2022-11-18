4bf699e0025b4d9909c11f60c1dd2a4bce867690
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-11-18 13:29:00 +0100

    Set WSL to UTF8 for coverage reports

--- coverage.ps1
+++ coverage.ps1
@@ -1,3 +1,4 @@
 $cov_dir=".\coverage"
+$env:WSL_UTF8=1
 go.exe test -covermode=set -coverprofile="${cov_dir}\coverage.out" -coverpkg=./...
 go.exe tool cover -o "${cov_dir}\index.html" -html="${cov_dir}\coverage.out"

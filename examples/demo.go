291869b7d27156bdc96d8adc9136fa0e23f0bbf8
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-01-18 09:56:00 +0100

    Made demo more accessible

--- examples/demo.go
+++ examples/demo.go
@@ -13,8 +13,7 @@ func main() {
 	distro := wsl.Distro{Name: "Ubuntu-22.04-test"}
 
 	// Registering a new distro
-	// TODO: install ubuntu-preview from the store?
-	fmt.Println("Registering a new WSL distro...")
+	fmt.Printf("Registering a new distro %q\n", distro.Name)
 	if err := distro.Register(`.\images\jammy.tar.gz`); err != nil {
 		panic(err)
 	}
@@ -23,33 +22,34 @@ func main() {
 	defer distro.Unregister()
 
 	// Getting config and printing it
+	fmt.Println("\nPrinting distro information:")
 	fmt.Printf("%v", distro)
 
 	// Setting config
+	fmt.Println("\nDisable windows paths and fail to run notepad:")
 	distro.PathAppended(false)
 
-	// Launching async command
-	process1 := distro.Command(context.Background(), `sleep 3 && cat goodmorning.txt`)
-	process1.Start()
-
-	// Launching async command
-	process2 := distro.Command(context.Background(), `echo "Hello, world from WSL!" > "goodmorning.txt"`)
-	process2.Run()
-
 	// Launching an interactive command (should fail as per config change)
-	err := distro.Shell(wsl.WithCommand("sh -c 'notepad.exe'"))
+	out, err := distro.Command(context.Background(), "sh -c 'notepad.exe'").CombinedOutput()
 	if err != nil {
-		fmt.Printf("Sync command unsuccesful: %v\n", err)
+		fmt.Printf("%v\n%s", err, out)
 	} else {
-		fmt.Printf("Sync command succesful\n")
+		fmt.Printf("Unexpected success:\n%s", out)
 	}
 
-	// Managing the result of the async commands
-	err = process1.Wait()
+	// Launching async command 1
+	fmt.Println("\nRunning async commands")
+	cmd1 := distro.Command(context.Background(), `sleep 3 && cat goodmorning.txt`)
+	cmd1.Start()
+
+	// Launching and waiting for command 2
+	distro.Command(context.Background(), `echo "Hello, world from WSL!" > "goodmorning.txt"`).Run()
+
+	// Waiting for command 1
+	err = cmd1.Wait()
 	if err != nil && !errors.Is(err, &wsl.ExitError{}) {
 		panic(err)
 	}
-
 	if err != nil {
 		fmt.Printf("Unsuccesful async command: %v\n", err)
 		return
@@ -57,6 +57,7 @@ func main() {
 	fmt.Printf("Succesful async command!\n")
 
 	// Showing CommandContext
+	fmt.Println("\nCancelling a command that takes too long")
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
@@ -65,5 +66,15 @@ func main() {
 		fmt.Printf("Process with timeout failed: %v\n", err)
 	} else {
 		fmt.Println("Process with timeout succeeded!")
+	}
+
+	// Starting Shell
+	fmt.Println("\nStarting a shell in Ubuntu. Feel free to exit to continue the demo")
+	fmt.Println()
+	err = distro.Shell()
+	if err != nil {
+		fmt.Printf("Shell exited with error:\n%s", err)
+	} else {
+		fmt.Println("Shell exited with exit code 0")
 	}
 }

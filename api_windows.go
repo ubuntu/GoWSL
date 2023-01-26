cee0c47f4bf9dbeca9c631bf01c1f1e5a6a8855f
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-01-26 09:58:06 +0100

    Created wsl's startProcess to mock os.StartProcess with the wslAPI

--- api_windows.go
+++ api_windows.go
@@ -3,6 +3,9 @@ package gowsl
 // This file contains windows-only API definitions and imports
 
 import (
+	"errors"
+	"fmt"
+	"os"
 	"syscall"
 	"unsafe"
 
@@ -33,4 +36,46 @@ type char = byte     // Windows' CHAR (which is the same as C's char)
 
 func coTaskMemFree(p unsafe.Pointer) {
 	windows.CoTaskMemFree(p)
+}
+
+// startProcess replaces os.StartProcess with WSL commands.
+func (c Cmd) startProcess() (process *os.Process, err error) {
+	distroUTF16, err := syscall.UTF16PtrFromString(c.distro.Name())
+	if err != nil {
+		return nil, errors.New("failed to convert distro name to UTF16")
+	}
+
+	commandUTF16, err := syscall.UTF16PtrFromString(c.command)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert command %q to UTF16", c.command)
+	}
+
+	var useCwd wBOOL
+	if c.UseCWD {
+		useCwd = 1
+	}
+
+	var handle windows.Handle
+	r1, _, _ := wslLaunch.Call(
+		uintptr(unsafe.Pointer(distroUTF16)),
+		uintptr(unsafe.Pointer(commandUTF16)),
+		uintptr(useCwd),
+		c.stdinR.Fd(),
+		c.stdoutW.Fd(),
+		c.stderrW.Fd(),
+		uintptr(unsafe.Pointer(&handle)))
+
+	if r1 != 0 {
+		return nil, fmt.Errorf("failed syscall to WslLaunch")
+	}
+	if handle == windows.Handle(0) {
+		return nil, fmt.Errorf("syscall to WslLaunch returned a null handle")
+	}
+
+	pid, err := windows.GetProcessId(handle)
+	if err != nil {
+		return nil, errors.New("failed to find launched process")
+	}
+
+	return os.FindProcess(int(pid))
 }

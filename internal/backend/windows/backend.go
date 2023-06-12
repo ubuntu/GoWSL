fd99dc07c8fbdf2a71e3b77c4d9a22e6fea3f56d
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-06-12 10:32:32 +0200

    Implement Windows uninstall back-end

--- internal/backend/windows/backend.go
+++ internal/backend/windows/backend.go
@@ -5,5 +5,30 @@
 // All functions will return an error when ran on Linux.
 package windows
 
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
 // Backend implements the Backend interface.
 type Backend struct{}
+
+// RemoveAppxFamily uninstalls the Appx under the provided family name.
+func (Backend) RemoveAppxFamily(ctx context.Context, packageFamilyName string) error {
+	cmd := exec.CommandContext(ctx,
+		"powershell.exe",
+		"-NonInteractive",
+		"-NoProfile",
+		"-NoLogo",
+		"-Command",
+		`Get-AppxPackage | Where-Object -Property PackageFamilyName -eq "${env:PackageFamilyName}" | Remove-AppPackage`,
+	)
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PackageFamilyName=%q", packageFamilyName))
+
+	if out, err := cmd.Output(); err != nil {
+		return fmt.Errorf("could not uninstall %q: %v. %s", packageFamilyName, err, out)
+	}
+
+	return nil
+}
